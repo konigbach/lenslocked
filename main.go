a2ccad80423b57dd6cd14506b99e2bc20efe1429
Daniel Sánchez <daniel.sanchez@bitpanda.com>
2024-02-07 10:08:45 +0100

    Routing

--- main.go
+++ main.go
@@ -5,12 +5,29 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; utf-8")
+	fmt.Fprintf(w, "<h1>Hello, World!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; utf-8")
+	fmt.Fprintf(w, "<h1>Hello, World1!</h1>")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting server...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe("localhost:3000", nil)
 }
