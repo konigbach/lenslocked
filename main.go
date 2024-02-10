d4d7b6dcf4c5fab424d2a8c76f9958bc3459dfa6
Daniel Sánchez <daniel.sanchez@bitpanda.com>
2024-02-10 18:30:00 +0100

    Refactor

--- main.go
+++ main.go
@@ -2,32 +2,56 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; utf-8")
-	fmt.Fprintf(w, "<h1>Hello, World!</h1>")
+	executeTemplate(w, "templates/home.gohtml")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; utf-8")
-	fmt.Fprintf(w, "<h1>Hello, World11!</h1>")
+	executeTemplate(w, "templates/contact.gohtml")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		http.Error(w, "Not Found", 404)
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; utf-8")
+	executeTemplate(w, "templates/faq.gohtml")
+}
+
+func executeTemplate(w http.ResponseWriter, filepath string) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		log.Printf("processing template: %v", err)
+		http.Error(w, "There was an error processing the template.", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
 	}
 }
 
 func main() {
+	r := chi.NewRouter()
+	r.Route("/contact", func(r chi.Router) {
+		r.Use(middleware.Logger)
+		r.Get("/", contactHandler)
+	})
+	r.Get("/", homeHandler)
+	r.Get("/faq", faqHandler)
+	r.Get("/testing/{name}", testingHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 	fmt.Println("Starting server...")
-	http.ListenAndServe("localhost:3000", http.HandlerFunc(pathHandler))
+	http.ListenAndServe("localhost:3000", r)
 }
